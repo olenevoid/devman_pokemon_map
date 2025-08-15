771069bd553979a0c1098a2fd5adb2a7945f0126
olenevoid <mazekunov@gmail.com>
2025-08-15 10:40:17 +0500

    Create pokemon model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,3 +1,6 @@
 from django.db import models  # noqa F401
 
 # your models here
+class Pokemon(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Имя')
+    image = models.ImageField(verbose_name='Картинка', upload_to='images/')
