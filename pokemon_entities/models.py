1761ef628a2e5388b33aad32ec8be3bac798f9e0
olenevoid <mazekunov@gmail.com>
2025-08-19 10:55:10 +0500

    Show pokemon name in admin UI

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,3 +4,6 @@ from django.db import models  # noqa F401
 class Pokemon(models.Model):
     name = models.CharField(max_length=200, verbose_name='Имя')
     image = models.ImageField(verbose_name='Картинка', upload_to='images/')
+
+    def __str__(self):
+        return self.name
