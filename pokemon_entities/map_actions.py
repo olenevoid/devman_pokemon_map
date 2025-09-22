68920b1a3767c7a039d861600c3e0b771a3720c0
olenevoid <mazekunov@gmail.com>
2025-09-22 12:11:34 +0500

    Move map related functions to map_actions.py

---
+++ pokemon_entities/map_actions.py
@@ -0,0 +1,36 @@
+import folium
+
+
+MOSCOW_CENTER = [55.751244, 37.618423]
+DEFAULT_IMAGE_URL = (
+    'https://vignette.wikia.nocookie.net/pokemon/images/6/6e/%21.png/revision'
+    '/latest/fixed-aspect-ratio-down/width/240/height/240?cb=20130525215832'
+    '&fill=transparent'
+)
+
+
+def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
+    icon = folium.features.CustomIcon(
+        image_url,
+        icon_size=(50, 50),
+    )
+    folium.Marker(
+        [lat, lon],
+        # Warning! `tooltip` attribute is disabled intentionally
+        # to fix strange folium cyrillic encoding bug
+        icon=icon,
+    ).add_to(folium_map)
+
+
+def fill_map_with_pokemons(pokemons, request, folium_map = None):
+    folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
+    for pokemon in pokemons:
+        for pokemon_entity in pokemon['entities']:
+            add_pokemon(
+                folium_map,
+                pokemon_entity['lat'],
+                pokemon_entity['lon'],
+                request.build_absolute_uri(pokemon['img_url'])
+            )
+
+    return folium_map
